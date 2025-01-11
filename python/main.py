17035b5ef14b816b30bce27b0557de8b5b835bf8
simsononroad <d-github@kopaszhegy.hu>
2025-01-11 18:24:21 +0100

    implement login functionality with Flask and add login page template

--- python/main.py
+++ python/main.py
@@ -1,5 +1,28 @@
-def main():
-    pass
+from flask import *
+
+
+app = Flask(__name__)
+app.secret_key = "szupertitkoskulcs"  # Ezt cseréld le egy erősebb kulcsra!
+
+
+
+# Előre meghatározott felhasználónév és jelszó
+
+
+# Főoldal (index)
+@app.route("/")
+def index():
+    return render_template("login_page.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username_html = request.form['username']
+    password_html = request.form['password']
+    
+    
+    
+    return redirect(url_for("index"))
+    
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, port=8080)
